3b08d7d0eb86407580d36ee40b9ffd388e06b1fb
Krzysztof Pieniążek <krzysztof.pieniazek@hltech.com>
2021-04-01 14:18:39 +0200

    Fix for checkstyle issue with lambda

--- src/main/java/com/hltech/store/AggregateRepository.java
+++ src/main/java/com/hltech/store/AggregateRepository.java
@@ -100,4 +100,3 @@ public class AggregateRepository<A, E> {
     }
 
 }
-
--- src/main/java/com/hltech/store/AggregateRepositoryException.java
+++ src/main/java/com/hltech/store/AggregateRepositoryException.java
@@ -1,7 +1,9 @@
 package com.hltech.store;
 
 class AggregateRepositoryException extends RuntimeException {
+
     AggregateRepositoryException(String message) {
         super(message);
     }
+
 }
