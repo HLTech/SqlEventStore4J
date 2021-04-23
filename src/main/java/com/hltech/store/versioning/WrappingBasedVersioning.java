cb2078411d50dd05375e1975e14e27eae772f011
michal.karolik <michal.karolik@hltech.com>
2021-04-23 15:15:31 +0200

    wrapping versioning functionality

--- src/main/java/com/hltech/store/versioning/WrappingBasedVersioning.java
+++ src/main/java/com/hltech/store/versioning/WrappingBasedVersioning.java
@@ -1,5 +1,15 @@
 package com.hltech.store.versioning;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES;
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+
 /**
  * In this strategy multiple versions of the event are wrapped, using registered wrapper.
  * Wrapper is a class that extends desirable event type and provides all data directly from wrapped json.
@@ -12,24 +22,60 @@ package com.hltech.store.versioning;
  */
 public class WrappingBasedVersioning<E> implements EventVersioningStrategy<E> {
 
+    private static final int CONSTANT_VERSION_NUMBER = 1;
+
+    private final Map<Class<? extends E>, String> eventTypeToNameMap = new HashMap<>();
+    private final Map<String, Function<String, E>> eventNameToWrapperMap = new HashMap<>();
+
+    @Getter
+    private final ObjectMapper objectMapper;
+
+    public WrappingBasedVersioning() {
+        objectMapper = new ObjectMapper();
+        objectMapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(FAIL_ON_MISSING_CREATOR_PROPERTIES, false);
+    }
+
     @Override
     public E toEvent(String eventJson, String eventName, int eventVersion) {
-        throw new IllegalStateException("Not yet implemented");
+        Function<String, E> wrapper = eventNameToWrapperMap.get(eventName);
+        try {
+            return wrapper.apply(eventJson);
+        } catch (Exception ex) {
+            throw new EventBodyMappingException(eventJson, eventName, ex);
+        }
     }
 
     @Override
     public String toName(Class<? extends E> eventType) {
-        throw new IllegalStateException("Not yet implemented");
+        String eventName = eventTypeToNameMap.get(eventType);
+        if (eventName == null) {
+            throw new EventTypeMappingException("Mapping to event name not found for event type: " + eventType);
+        }
+        return eventName;
     }
 
     @Override
     public int toVersion(Class<? extends E> eventType) {
-        throw new IllegalStateException("Not yet implemented");
+        return CONSTANT_VERSION_NUMBER;
     }
 
     @Override
     public String toJson(E event) {
-        throw new IllegalStateException("Not yet implemented");
+        try {
+            return objectMapper.writeValueAsString(event);
+        } catch (Exception ex) {
+            throw new EventBodyMappingException(event, ex);
+        }
+    }
+
+    public void registerEvent(
+            Class<? extends E> eventType,
+            String eventName,
+            Function<String, E> wrapper
+    ) {
+        eventTypeToNameMap.put(eventType, eventName);
+        eventNameToWrapperMap.put(eventName, wrapper);
     }
 
 }
